1c812178350ef8e4d541c3075cd5f8c73f0f9f84
rpz80 <rippa80@gmail.com>
2017-01-11 09:25:33 +0300

    in progress

--- log/utils.h
+++ log/utils.h
@@ -13,9 +13,28 @@ std::string join(const std::string& subPath1,
 }
 
 namespace str {
+namespace detail {
 template<typename... Args>
-std::string join(Args&&... args) {
+size_t calcSize(const Args&... args);
+
+template<typename... Tail>
+size_t calcSize(const std::string& head, const Tail&... tail) {
+  return head.size() + calcSize(tail...);
 }
+
+template<size_t N, typename... Tail>
+size_t calcSize(const char const(&) [N], const Tail&... tail) {
+  return N - 1 + calcSize(tail...);
+}
+}
+
+template<typename... Args>
+std::string join(const Args&... args) {
+  std::string result;
+  result.reserve(detail::calcSize(args...));
+  str::detail::fillResult(result, tail...);
+}
+
 }
 
 }
