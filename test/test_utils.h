ff9e80ece7747ed0d4bcc9080fefe5a9da39572e
Alexander Kulikov <rippa80@gmail.com>
2017-02-09 20:31:59 +0300

    in progress

--- test/test_utils.h
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include <sys/stat.h>
-#include <dirent.h>
-
-bool removeDir(const char* dirName);
-bool fileExists(const char* name);
-const char* catFileName(char* buffer, const char* dirName, const char* fname);
-const char* fileContent(char* buf, const char* fileName);
